0c66c15f0b83373dbdee67cea2f9d9066cc45175
lielongxingkong <zzl_1164@126.com>
2014-03-19 10:10:22 -0400

    add var name for prototype

--- ring_buffer/include/ring_api.h
+++ ring_buffer/include/ring_api.h
@@ -17,16 +17,16 @@ struct ringbuf{
 	struct rw_semaphore;
 };
 
-int init_ringbuf(struct *ringbuf);
-int clean_ringbuf(struct *ringbuf);
-bool full(struct *ringbuf);
-bool empty(struct *ringbuf);
-int push(struct *ringbuf, struct elem);
-struct elem pop(struct *ringbuf);
+int init_ringbuf(struct *ringbuf ring);
+int clean_ringbuf(struct *ringbuf ring);
+bool full(struct *ringbuf ring);
+bool empty(struct *ringbuf ring);
+int push(struct *ringbuf ring, struct elem e);
+struct elem pop(struct *ringbuf ring);
 
 /*call when full*/
-int extend(struct *ringbuf, int extend_len);
+int extend(struct *ringbuf ring, int extend_len);
 /*push no lock*/
-int __push(struct *ringbuf, struct elem);
-/*pop no lock*/
-struct elem __pop(struct *ringbuf);
+int __push(struct *ringbuf ring, struct elem e);
+/*poo no lock*/
+struct elem pop(struct *ringbuf ring);
