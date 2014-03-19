dcd3406d69a11d91cb7a255446cf038ee4d194be
lielongxingkong <zzl_1164@126.com>
2014-03-19 10:26:37 -0400

    basic func skeleton

--- ring_buffer/ring/ring_api.c
+++ ring_buffer/ring/ring_api.c
@@ -1,5 +1,55 @@
 #include <linux/slab.h>
-int test(void)
+#include <ring_api.h>
+
+
+int init_ringbuf(struct ringbuf *ring)
 {
-	return 0;
+		return 0;	
+}
+
+int clean_ringbuf(struct ringbuf *ring)
+{
+		return 0;
+}
+
+bool full(struct ringbuf *ring)
+{
+		return 0;
+}
+
+bool empty(struct ringbuf *ring)
+{
+		return 0;
+}
+
+int push(struct ringbuf *ring, struct elem e)
+{
+		return 0;
+}
+
+struct elem pop(struct ringbuf *ring)
+{
+		struct elem e;
+		e.data = 1;
+		return e;
+}
+
+/*call when full*/
+int extend(struct ringbuf *ring, int extend_len)
+{
+		return 0;
+}
+
+/*push no lock*/
+int __push(struct ringbuf *ring, struct elem e)
+{
+		return 0;
+}
+
+/*pop no lock*/
+struct elem __pop(struct ringbuf *ring)
+{
+		struct elem e;
+		e.data = 1;
+		return e;
 }
