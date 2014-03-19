e9aa84c9c9b3771c89cd6be23e6dcc3032b32ad4
lielongxingkong <zzl_1164@126.com>
2014-03-19 10:04:34 -0400

    init version ringbuf api

--- ring_buffer/include/ring_api.h
+++ ring_buffer/include/ring_api.h
@@ -1,12 +1,32 @@
-struct ring_node{
-	struct ring_node *next;
-	struct ring_node *priv;
-	void *data;
+#include <linux/list.h>
+#include <linux/rwsem.h>
+struct elem{
+	int data;
 };
-struct ring_buf{
-	void *buf;
+
+struct node{
+	struct elem;
+	struct list_head list;
+};
+
+struct ringbuf{
+	struct node *buf;
 	int entries;
-	spinlock_t lock;
-
-
+	struct node *front;
+	struct node *rear;
+	struct rw_semaphore;
 };
+
+int init_ringbuf(struct *ringbuf);
+int clean_ringbuf(struct *ringbuf);
+bool full(struct *ringbuf);
+bool empty(struct *ringbuf);
+int push(struct *ringbuf, struct elem);
+struct elem pop(struct *ringbuf);
+
+/*call when full*/
+int extend(struct *ringbuf, int extend_len);
+/*push no lock*/
+int __push(struct *ringbuf, struct elem);
+/*pop no lock*/
+struct elem __pop(struct *ringbuf);
