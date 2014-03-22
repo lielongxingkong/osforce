5277eca5a9e170e116e961344b4d1fbc0efbbe2a
Zhao Zhenlong <zzl_1164@126.com>
2014-03-22 10:41:43 -0400

    add print_circle to test

--- ring_buffer/test/ring.c
+++ ring_buffer/test/ring.c
@@ -10,10 +10,24 @@ void print(struct ringbuf ring)
 	printk("[DEBUG end] ----------------\n");
 }
 
+void print_circle(struct ringbuf ring, int times)
+{
+	int i;
+	struct list_head *ptr;
+	ptr = ring.head;
+	for(i = 0; i < times * INIT_LEN; i++){
+		struct ringnode *entry;
+		entry = list_entry(ptr, struct ringnode, list);
+		printk("[DEBUG circle]%d:%d\n", i, entry->e.data);
+		ptr = ptr->next;
+	}
+}
+
 void test(struct ringbuf ring)
 {
 	init_ringbuf(&ring);
 	print(ring);
+	print_circle(ring, 2);
 
 	cleanup_ringbuf(&ring);
 	print(ring);
