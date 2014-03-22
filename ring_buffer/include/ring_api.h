4d29cd81b5df7734c76d562513bd289801fdd2cc
Zhao Zhenlong <zzl_1164@126.com>
2014-03-22 10:15:50 -0400

    change ringbuf structure and change push and pop api

--- ring_buffer/include/ring_api.h
+++ ring_buffer/include/ring_api.h
@@ -3,7 +3,8 @@
 #include <linux/err.h>
 #include <linux/rwsem.h>
 #include <linux/semaphore.h>
-#define INIT_LENGTH	10
+#define INIT_LEN	10
+#define EXTEND_LEN	10
 
 struct elem{
 	int data;
@@ -16,9 +17,9 @@ struct ringnode{
 
 struct ringbuf{
 	int entries;
-	struct list_head head;
-	struct list_head front;
-	struct list_head rear;
+	struct list_head *head;
+	struct list_head *front;
+	struct list_head *rear;
 	struct rw_semaphore rwsem;
 };
 
@@ -26,12 +27,12 @@ int init_ringbuf(struct ringbuf *ring);
 int cleanup_ringbuf(struct ringbuf *ring);
 bool full(struct ringbuf *ring);
 bool empty(struct ringbuf *ring);
-int push(struct ringbuf *ring, struct elem e);
-struct elem pop(struct ringbuf *ring);
+int push(struct ringbuf *ring, struct elem *e);
+int pop(struct ringbuf *ring, struct elem *e);
 
 /*call when full*/
 int extend(struct ringbuf *ring, int extend_len);
 /*push no lock*/
-int __push(struct ringbuf *ring, struct elem e);
-/*poo no lock*/
-struct elem pop(struct ringbuf *ring);
+int __push(struct ringbuf *ring, struct elem *e);
+/*pop no lock*/
+int __pop(struct ringbuf *ring, struct elem *e);
