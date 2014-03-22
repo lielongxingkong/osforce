190bc11ba5176ec4e2c5a246d46af8175e122455
Zhao Zhenlong <zzl_1164@126.com>
2014-03-22 10:17:43 -0400

    modify and add interface implementation

--- ring_buffer/ring/ring_api.c
+++ ring_buffer/ring/ring_api.c
@@ -3,19 +3,26 @@
 int init_ringbuf(struct ringbuf *ring)
 {
 	int i;
-	INIT_LIST_HEAD(&ring->head);	
-	for(i = 0; i < INIT_LENGTH; i++){
+	struct ringnode *head;
+	head = (struct ringnode*)kmalloc(sizeof(struct ringnode), GFP_KERNEL);
+	if(!head)
+		return -1;
+	memset(head, 0, sizeof(struct ringnode));
+	head->e.data = 0;
+	INIT_LIST_HEAD(&head->list);	
+	for(i = 1; i < INIT_LEN; i++){
 		struct ringnode *ptr;
 		ptr = (struct ringnode*)kmalloc(sizeof(struct ringnode), GFP_KERNEL);
 		if(!ptr)
 			return -1;
 		memset(ptr, 0, sizeof(struct ringnode));
 		ptr->e.data = i;
-		list_add(&ptr->list, &ring->head);
+		list_add(&ptr->list, &head->list);
 	}
 
 	init_rwsem(&ring->rwsem);	
-	ring->entries = INIT_LENGTH;
+	ring->entries = INIT_LEN;
+	ring->head = &head->list;
 	ring->front = ring->head;
 	ring->rear = ring->head;
 	return 0;	
@@ -24,22 +31,31 @@ int init_ringbuf(struct ringbuf *ring)
 int cleanup_ringbuf(struct ringbuf *ring)
 {
 	struct ringnode *next, *ptr;
-	list_for_each_entry_safe(ptr, next, &ring->head, list) {
+	list_for_each_entry_safe(ptr, next, ring->head, list) {
 		list_del(&ptr->list);
 		kfree(ptr);
 	}
 	return 0;
 }
+
 bool full(struct ringbuf *ring)
 {
-	return 0;
+	if(ring->rear->next == ring->front)
+		return 1;
+	else
+		return 0;
 }
 
+/* front->next == rear */
 bool empty(struct ringbuf *ring)
 {
-	return 0;
+	if(ring->front->next == ring->rear)
+		return 1;
+	else
+		return 0;
 }
 
+/* rear->next == front */
 int push(struct ringbuf *ring, struct elem e)
 {
 	return 0;
@@ -55,19 +71,41 @@ struct elem pop(struct ringbuf *ring)
 /*call when full*/
 int extend(struct ringbuf *ring, int extend_len)
 {
+	int i;
+	for(i = 0; i < extend_len; i++){
+		struct ringnode *ptr;
+		ptr = (struct ringnode*)kmalloc(sizeof(struct ringnode), GFP_KERNEL);
+		if(!ptr)
+			return -1;
+		memset(ptr, 0, sizeof(struct ringnode));
+		ptr->e.data = i + ring->entries;
+		list_add(&ptr->list, ring->head);
+		ring->entries++;
+	}
 	return 0;
 }
 
 /*push no lock*/
-int __push(struct ringbuf *ring, struct elem e)
+int __push(struct ringbuf *ring, struct elem *e)
 {
+	struct ringnode *entry;
+	if(full(ring))
+		if(extend(ring, EXTEND_LEN))
+			return -1;
+	entry = list_entry(ring->rear->next, struct ringnode, list);
+	entry->e.data = e->data;	
+	ring->rear = ring->rear->next;
 	return 0;
 }
 
 /*pop no lock*/
-struct elem __pop(struct ringbuf *ring)
+int __pop(struct ringbuf *ring, struct elem *e)
 {
-	struct elem e;
-	e.data = 1;
-	return e;
+	struct ringnode *entry;
+	if(empty(ring))
+		return -1;
+	entry = list_entry(ring->front, struct ringnode, list);
+	e->data = entry->e.data;	
+	ring->front = ring->front->next;
+	return 0;
 }
